d02e1f4d2cabb7b5b5957f381c32138105617a1e
Samuel BUCHER <samuel.bucher@webappfactory.fr>
2026-09-19 07:10:49 +0200

    Add void return type to Extension::load() for Symfony 8
    ExtensionInterface::load() now declares a strict void return type.

--- src/DependencyInjection/SymfonyCrudExtension.php
+++ src/DependencyInjection/SymfonyCrudExtension.php
@@ -12,7 +12,7 @@ class SymfonyCrudExtension extends Extension
     /**
      * {@inheritdoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $configuration = new Configuration();
         $this->processConfiguration($configuration, $configs);
